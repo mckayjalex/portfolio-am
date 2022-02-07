445272cae33462652926d407e656a5f741ee24d7
Alex <chaza.mckay@me.com>
2022-02-07 17:54:59 +1030

    Final update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+let dropdownBtn = document.querySelector('#dropdownButton');
+let dropdown = document.querySelector('#dropdown');
+let list = document.querySelector('#list');
+let main = document.querySelector('#main');
 
+dropdownButton.addEventListener('click', function() {
+    dropdown.classList.remove('hidden');
+    dropdownBtn.classList.add('hidden')
+})
+list.addEventListener('click', function() {
+    dropdownBtn.classList.remove('hidden')
+    dropdown.classList.add('hidden');
+})
+main.addEventListener('click', function() {
+    dropdownBtn.classList.remove('hidden')
+    dropdown.classList.add('hidden');
+})
